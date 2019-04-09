4de5058901b94837dab9bef5bb0091791751169a
Ethan Choi <jychoi.ethan@gmail.com>
2019-04-09 20:20:16 +0900

    Initial updates

---
+++ model.py
@@ -0,0 +1,93 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+from collections import deque
+import matplotlib.pyplot as plt
+
+class QNetwork(nn.Module):
+    """Actor (Policy) Model."""
+    def __init__(self, state_size, action_size, seed, hidden_layers=[256,128]):
+        """Initialize parameters and build model.
+        Params
+        ======
+            state_size (int): Dimension of each state
+            action_size (int): Dimension of each action
+            seed (int): Random seed
+            hidden_layers : list of integers that indicates the size of each hidden layers, default=[256,128]
+        """
+        super(QNetwork, self).__init__()
+        self.seed = torch.manual_seed(seed)
+
+        self.layers = nn.ModuleList([nn.Linear(state_size, hidden_layers[0])])
+        layer_sizes = zip(hidden_layers[:-1], hidden_layers[1:])
+        self.layers.extend([nn.Linear(h1, h2) for h1, h2 in layer_sizes])
+
+        self.output = nn.Linear(hidden_layers[-1], action_size)
+
+    def forward(self, state):
+        """Build a network that maps state -> action values."""
+        x = state
+        for linear in self.layers:
+            x = F.relu(linear(x))
+        x = self.output(x)
+        return x
+
+
+def dqn(env, agent, checkpoint="checkpoint.pth", n_episodes=1800, max_t=1000, eps_start=1.0, eps_end=0.01, eps_decay=0.995):
+    """Deep Q-Learning.
+    
+    Params
+    ======
+        env : Environment
+        agent : Agent to train
+        checkpoint : file to save checkpoint
+        n_episodes (int): maximum number of training episodes
+        max_t (int): maximum number of timesteps per episode
+        eps_start (float): starting value of epsilon, for epsilon-greedy action selection
+        eps_end (float): minimum value of epsilon
+        eps_decay (float): multiplicative factor (per episode) for decreasing epsilon
+    """
+    scores = []                        # list containing scores from each episode
+    scores_window = deque(maxlen=100)  # last 100 scores
+    eps = eps_start                    # initialize epsilon
+    brain_name = env.brain_names[0]
+
+    for i_episode in range(1, n_episodes+1):
+        env_info = env.reset(train_mode=True)[brain_name]
+        state = env_info.vector_observations[0] 
+        score = 0
+        for t in range(max_t):
+            action = agent.act(state, eps)
+            env_info = env.step(action)[brain_name]
+            
+            next_state = env_info.vector_observations[0]
+            reward = env_info.rewards[0]
+            done = env_info.local_done[0]
+    
+            agent.step(state, action, reward, next_state, done)
+            state = next_state
+            score += reward
+            if done:
+                break 
+        scores_window.append(score)       # save most recent score
+        scores.append(score)              # save most recent score
+        eps = max(eps_end, eps_decay*eps) # decrease epsilon
+        print('\rEpisode {}\tAverage Score: {:.2f}'.format(i_episode, np.mean(scores_window)), end="")
+        if i_episode % 100 == 0:
+            print('\rEpisode {}\tAverage Score: {:.2f}'.format(i_episode, np.mean(scores_window)))
+        if np.mean(scores_window)>=13.0:
+            print('\nEnvironment solved in {:d} episodes!\tAverage Score: {:.2f}'.format(i_episode-100, np.mean(scores_window)))
+            torch.save(agent.qnetwork_local.state_dict(), checkpoint)
+            break
+    return scores
+
+def plot_scores(scores):
+    """
+    plot the scores
+    """
+    fig = plt.figure()
+    plt.plot(np.arange(len(scores)), scores)
+    plt.ylabel('Score')
+    plt.xlabel('Episode #')
+    plt.show()
